27aadf37d9784e53c4c479d5fcca71e1e9d3928b
Mcanroe <mcanroe21@gmail.com>
2024-12-08 11:47:12 +0000

    merge got cooked , fixed now

--- day3/day3.py
+++ day3/day3.py
@@ -2,21 +2,12 @@ from pathlib import Path
 import re
 
 p = Path("day3\day3input.txt")
-csum = 0
-pos = 0
-
-
-def cumu_sum (lst):
-    global csum
-    for i in range(len(lst)):
-        match = re.findall("\d+", lst[i])
-        csum = csum + (int(match[0]) * int(match[1]))
-    return csum
+mult_sum = 0
 
 with p.open("r") as src:
     test = src.read()
-    mult_list = re.findall("mul\(\d+,\d+\)",test)
+    mult_list = re.findall("mul\(\d+,\d+\)", test)
     for i in range(len(mult_list)):
-        match = re.findall(r"\d+",mult_list[i])    
+        match = re.findall(r"\d+", mult_list[i])
         mult_sum = mult_sum + (int(match[0]) * int(match[1]))
-        print(mult_sum)
+    print(mult_sum)
